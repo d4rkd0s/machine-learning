f2fd1f363982a1fe910816318ea0d2ce49843cab
Logan Schmidt <d4rkd0s@gmail.com>
2022-04-19 01:48:50 -0500

    Added a scalar, vector, and matrix

--- 2022-April/10-ztm.py
+++ 2022-April/10-ztm.py
@@ -1,3 +1,17 @@
 # Import TensorFlow
 import tensorflow as tf
 print(tf.__version__) # find the version number (should be 2.x+)
+
+# Creating tensors with tf.constant
+
+# Create a scalar tensor 
+scalar = tf.constant(1337)
+print(scalar)
+
+# Create a vector tensor
+vector = tf.constant([1, 2, 3, 4, 5])
+print(vector)
+
+# Create a matrix tensor
+matrix = tf.constant([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(matrix)
